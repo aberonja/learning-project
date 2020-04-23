e011d97108260f79b2041ef7aaad80f53ea74dfa
bulic <bulic@execom.eu>
2020-04-23 15:36:56 +0200

    change function use ES6

--- indexB.js
+++ indexB.js
@@ -1,114 +1,88 @@
 //assignment1
-let arrVege = ['isApple', 'isPear', 'isCucumber', 'isMelon'];
-const boolRandom = () => {
-  let res = [];
-  for (let i = 0; i < arrVege.length; i++) {
-    obj = { name: arrVege[i], bool: Math.random() >= 0.5 };
-    res.push(obj);
-  }
-  return res;
-};
-console.log(' (1) ', boolRandom());
+const arrVege = ['isApple', 'isPear', 'isCucumber', 'isMelon'];
+const randomValues = (arr) =>
+  arr.reduce((obj, el) => ((obj[el] = !Math.round(Math.random())), obj), {});
+
+console.log('(1)', randomValues(arrVege));
 
 //assignment2
 const arrObj = [true, true, false, true, true];
-const objectTrue = (arr) => {
+const objectValidation = (arr) => {
   const validation = [];
   arr.forEach((a) => validation.push({ isTrue: a }));
   return validation;
 };
-console.log('(2)', objectTrue(arrObj));
 
-// assignement 3
+console.log('(2)', objectValidation(arrObj));
+
+//assignment3
 const objectName = { numberOne: 'Luigi', numberTwo: 'Mario' };
-const arrayObj = () => {
-  let arr1 = [];
-  arr1.push('numberOne');
-  arr1.push(objectName.numberOne);
-  let arr2 = [];
-  arr2.push('numberTwo');
-  arr2.push(objectName.numberTwo);
-  let arr = [arr1, arr2];
+const arrayObj = (obj) => {
+  const arr = Object.entries(obj);
   return arr;
 };
-console.log('(3)', arrayObj());
+
+console.log('(3)', arrayObj(objectName));
 
 // assignement 4
 var objectProp = [
   { name: 'Luigi', status: 'Number one' },
   { name: 'Mario', status: 'Number two' },
 ];
-const addNewProp = () => {
-  objectProp.forEach((v) => {
+const addNewProp = (arr) => {
+  arr.map((v, i) => {
     v.label = v.name;
-    v.value = v.status;
-    if (v.status === 'Number one') {
-      v.value = 1;
-    } else {
-      v.value = 2;
-    }
+    v.value = i + 1;
   });
-
-  return objectProp;
+  return arr;
 };
-console.log('(4)', addNewProp());
+
+console.log('(4)', addNewProp(objectProp));
 
 //assignement 5
-const arrayLessThenTen = (arr3, arr4) => {
-  let array3 = arr3.concat(arr4);
-  let newArray = [];
-  for (let i = 0; i < array3.length; i++)
-    if (array3[i] < 10) {
-      newArray.push(array3[i]);
-    }
+const arr1 = [1, 2, 3];
+const arr2 = [100, 2, 1, 10];
+const arrayLessThenTen = (arr1, arr2) => {
+  const newArray = [...arr1, ...arr2].filter((el) => el < 10);
   return newArray;
 };
-console.log(' (5) ' + arrayLessThenTen([1, 2, 3], [100, 2, 1, 10]));
+
+console.log(' (5) ', arrayLessThenTen(arr1, arr2));
 
 // assignement 6
 var res = [
   { name: 'Luigi', status: 'Number one' },
   { name: 'Mario', status: 'Number two' },
 ];
-const nameMatch = () => {
-  let k = -1;
-  let i = 0;
-  while (i < res.length && k < 0) {
-    if (res[i].name == 'Mario') {
-      k = i;
-    }
-    i++;
-  }
-  return res[k];
+const nameMatch = (arr) => {
+  let newArr = arr.find((x) => x.name === 'Mario');
+  return newArr;
 };
-console.log('(6) ', nameMatch());
+
+console.log('(6) ', nameMatch(res));
 
 // assignement 7
-const arrayCon = () => {
-  let arr1 = [1, 7, 3];
-  let arr2 = [1060, 2, 15, 10];
-  let concatArrays = arr1.concat(arr2);
-  let arrayUnion = [];
-  arrayUnion.push(concatArrays);
-  return arrayUnion;
+const arrFirst = [1, 7, 3];
+const arrSecond = [1060, 2, 15, 10];
+const arrayCon = (arr1, arr2) => {
+  let concatArrays = [];
+  concatArrays = [...arr1, ...arr2];
+  return concatArrays;
 };
-console.log(' (7) ', arrayCon());
+
+console.log(' (7) ', arrayCon(arrFirst, arrSecond));
 
 // assignement 8
-arr = [NaN, 1, 'null', 0, '0', 15, false, -22, '', undefined, 47, null];
+const arr = [NaN, 1, 'null', 0, '0', 15, false, -22, '', undefined, 47, null];
 const withoutNan = (arr) => {
-  let newArray = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i]) {
-      newArray.push(arr[i]);
-    }
-  }
+  const newArray = arr.filter((a, i) => arr[i]);
   return newArray;
 };
+
 console.log(' (8) ', withoutNan(arr));
 
 // assignement 9
-arr = [
+const arrOfNumbers = [
   1,
   3,
   44,
@@ -132,17 +106,31 @@ arr = [
   8,
   44,
 ];
-let newArray1 = [];
-for (let i = 0; i < arr.length; i++) {
-  if (arr[i] % 2 && arr[i] > 50) newArray1.push(arr[i]);
-}
-console.log(' (9) ', newArray1);
+
+const newArray = arrOfNumbers.filter((el) => el % 2 != 0 && el > 50);
+
+console.log(' (9) ', newArray);
 
 // assignement 10
-let newArray2 = arr.map((e) => {
+const newArray2 = arrOfNumbers.map((e) => {
   return e * 2;
 });
+
 console.log(' (10) ', newArray2);
+
+const newArray3 = Array.from(arrOfNumbers, (x) => x + x);
+
+console.log(' (10a) ', newArray3);
+
+const doubleArr = (array) => {
+  const newArr = [];
+  for (let i = 0; i < array.length; i++) {
+    newArr[i] = array[i] * 2;
+  }
+  return newArr;
+};
+
+console.log(' (10b) ', doubleArr(arrOfNumbers));
 
 // assignement 11
 const object = [
@@ -150,36 +138,37 @@ const object = [
   { Name: 'Bo', lastName: 'Smith', Age: 27, Capital: 'Belgrade' },
   { Name: 'Max', lastName: 'JoJo', Age: 29, Capital: 'London' },
 ];
-
-let nameAge = object.map((element) => {
-  let newArr = [];
-  newArr.push(element.Name, element.Age);
-  return newArr;
+const keyArray = object.map((item) => {
+  const newArrObj = {
+    fullName: ` ${item['Name']} ${item['lastName']} `,
+    age: item['Age'],
+  };
+  return newArrObj;
 });
-console.log('(11)', nameAge);
+
+console.log('(11)', keyArray);
 
 // assignement 12
-let array2 = [1, 4, 2, 3, 1, 5, 7, 3, 1];
+const arrayNumb = [1, 4, 2, 3, 1, 5, 7, 3, 1];
 const removeDuplicate = (data) => {
   return data.filter((value, index) => data.indexOf(value) === index);
 };
-console.log(' (12) ', removeDuplicate(array2));
+
+console.log(' (12) ', removeDuplicate(arrayNumb));
 
 // assignement 13
 const arrNumb = [10, 34, 15, 30, 1, 50, 75, 38, 19];
 const dividableByTen = (arrNumb) => arrNumb.filter((val) => val % 10 == 0);
+
 console.log(' (13) ', dividableByTen(arrNumb));
 
 // assignement 14
 const arrMaxMin = [10, 34, 15, 30, 1, 50, 75, 38, 19];
 const smallestBiggest = (arr) => {
-  let newArr = [];
-  arr.forEach(() => {
-    newArr[0] = Math.min.apply(Math, arr);
-    newArr[1] = Math.max.apply(Math, arr);
-  });
+  const newArr = [Math.min(...arr), Math.max(...arr)];
   return newArr;
 };
+
 console.log(' (14) ', smallestBiggest(arrMaxMin));
 
 // assignement 15
@@ -191,6 +180,7 @@ const reverseString = (string) => {
   }
   return newString;
 };
+
 console.log(' (15) ', reverseString(word));
 
 // assignement 16
@@ -199,33 +189,31 @@ const reverseStringRecursion = (str) => {
   str === '' ? '' : reverseStringRecursion(str.substr(1)) + str.charAt(0);
   return str;
 };
+
 console.log(' (16) ', reverseStringRecursion(word1));
 
 // assignement 17
+const array = [1, 6, 8, 12, 48];
 const minEvenNumb = (arr) => {
-  let newArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] % 2 === 0) {
-      newArr.push(arr[i]);
-    }
-  }
-  return Math.min(...newArr);
+  let array = Math.min(...arr.filter((arr) => arr % 2 === 0));
+  return array;
 };
-console.log(' (17) ', minEvenNumb([1, 6, 8, 12, 48]));
+
+console.log(' (17) ', minEvenNumb(array));
 
 // assignement 18
 const range = (start, end) => {
-  return Array(end - start + 1)
-    .fill()
-    .map((_, idx) => start + idx);
+  let array = Array.from(Array(end - start + 1), (_, i) => start + i * 1);
+  return array;
 };
-let resultOfRange = range(6, 15);
-console.log(' (18) ', resultOfRange);
+
+console.log(' (18) ', range(6, 15));
 
 // assignement 19
 const randomInteger = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+
 console.log(' (19) ', randomInteger(7, 20));
 
 // assignement 20
@@ -233,11 +221,14 @@ const arrayConcat = (arr1, arr2) => {
   let array = [...new Set([...arr1, ...arr2])];
   return array;
 };
+
 console.log(' (20) ', arrayConcat([1, 4, 2, 3, 1], [1, 6, 8, 2]));
 
 // assignement 21
-const shuffle = (arr) => {
-  let newArr = arr.sort(() => Math.random() - 0.5);
-  return newArr;
+const numbers = [1, 4, 2, 3, 1, 34, 75, 90];
+const shuffle = (arrS) => {
+  const shuffArr = arrS.sort(() => Math.random() - 0.5);
+  return shuffArr;
 };
-console.log(' (21) ', shuffle([1, 4, 2, 3, 1, 34, 75, 90]));
+
+console.log(' (21) ', shuffle(numbers));
